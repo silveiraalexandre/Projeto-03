fdbfc7f0f2797aa58a13748b323090cfea1ea4f8
FelipeEFreitas <felipeedefreitas@gmail.com>
2023-04-17 02:13:20 -0300

    Atualizações

--- DB/db.js
+++ DB/db.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 const sequelize = new Sequelize('projeto-3', 'root', 'mysql', {
   dialect: 'mysql',
   host: 'localhost',
-  port: 3306
+  port: 3000
 });
 
 module.exports = sequelize;
