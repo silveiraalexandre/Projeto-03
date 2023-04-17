0b1dc8148752d54c5b503d28e3349edc4ff3b536
Patrícia Gheller <121643390+PatriciaGheller@users.noreply.github.com>
2023-04-17 14:56:19 -0300

    atualizações index.js

---
+++ routes/index.js
@@ -0,0 +1,27 @@
+import Sequelize from "sequelize";
+
+const DB_NAME = "projeto-03"
+const DB_USER = "root"
+const DB_DB_PASSWORD = "mysql"
+const DB_CONFIG = {
+  dialect: "mysql",
+  host: "localhost",
+  port:3306,
+}
+
+const sequelize = new Sequelize(DB_NAME, DB_USER, DB_DB_PASSWORD, DB_CONFIG);
+
+const Psicologo = sequelize.define('Psicologo', { name: Sequelize.STRING });
+const Paciente = sequelize.define('Paciente', { name: Sequelize.STRING });
+const Atendimento = sequelize.define('Atendimento', {
+ 
+  PsicologoId: {
+    type: Sequelize.INTEGER,
+    references: {
+      model: Psicologo,
+      key: 'id'
+    }
+  }
+});
+Psicologo.belongsToMany(Paciente, { through: Atendimento });
+Paciente.belongsToMany(Psicologo, { through: Atendimento });
