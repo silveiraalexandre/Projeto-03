756f579880247bcc27347f6df64920c49df9acfe
Patrícia Gheller <121643390+PatriciaGheller@users.noreply.github.com>
2023-04-17 20:44:25 -0300

    atualizações app.js

--- app.js
+++ app.js
@@ -1,18 +1,11 @@
-const express = require("express");
+const express = require ("express");
 const routes = require("./routes");
 const db = require("./src/connection");
 const app = express();
 
-const app = express();
-db.hasConnection();
-
 app.use(express.json());
-
-app.use(express.json());
-
 app.use(routes);
 
-app.listen(3000,() => {
-    console.log('Servidor rodando na porta 3000')
+app.listen(3000, () => {
+    console.log('Servidor rodando na porta 3000');
 });
-
