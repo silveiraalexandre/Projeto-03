b639da03a71203cd280965da3a874d981fe05d44
FelipeEFreitas <felipeedefreitas@gmail.com>
2023-04-17 01:41:57 -0300

    Renomeado app.js para index.js

--- app.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const app = express();
-app.listen(3000, () => console.log("Servidor rodando na porta 3000"));
-
-app.get("/", (req, res) => {
-    res.send("Hello World!");
-    }
-);
-//fim d
