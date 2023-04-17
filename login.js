2551973ba951d192b4aac337da853d6470e6be9d
Raytriani25 <122921405+Raytriani25@users.noreply.github.com>
2023-04-17 06:37:27 -0300

    Add files via upload

---
+++ login.js
@@ -0,0 +1,12 @@
+const { validate, Joi} = require("express-validation");
+
+module.exports = validate({
+
+    body: Joi.object({
+
+        email: Joi.string().email().required(),
+
+        senha: Joi.string().required()
+    }),
+    
+});
